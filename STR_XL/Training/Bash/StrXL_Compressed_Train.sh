4f3dd639aa0de8f6037130ee44b0ca297b6e6190
Kendra  Givens <klg6z@hamilton04.cs.mtsu.edu>
2022-08-28 19:47:56 -0500

    Conquered the blue wall :0

--- STR_XL/Training/Bash/StrXL_Compressed_Train.sh
+++ STR_XL/Training/Bash/StrXL_Compressed_Train.sh
@@ -3,5 +3,5 @@
 export WANDB_DISABLED=FALSE
 export WANDB_ARTIFACTS_PATH=/data/klg6z/artifacts
 
-python3 StrXL_Compressed_Training.py --dataset-artifact sirdavidludwig/nachusa-dna/dnasamples-complete:latest --encoder-artifact sirdavidludwig/dnabert-pretrain/dnabert-pretrain-8dim:latest --wandb-project "StrXL_Compressed" --save_to "../../Weights/StrXL_Compressed_Run1" --gpus 0
+python3 StrXL_Compressed_Training.py --dataset-artifact sirdavidludwig/nachusa-dna/dnasamples-complete:latest --encoder-artifact sirdavidludwig/dnabert-pretrain/dnabert-pretrain-64dim:latest --wandb-project "StrXL_Compressed" --save_to "../../Weights/StrXL_Compressed_Run2" --gpus 0
 
