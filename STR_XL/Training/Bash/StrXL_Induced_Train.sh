faa5bfadc2e88dc168b8df7c340a0ae9d68921a3
Kendra <kendralee157@gmail.com>
2022-10-02 12:17:34 -0500

    Training scripts

--- STR_XL/Training/Bash/StrXL_Induced_Train.sh
+++ STR_XL/Training/Bash/StrXL_Induced_Train.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 StrXL_Induced_Training.py --dataset-artifact sirdavidludwig/nachusa-dna/dnasamples-complete:latest --encoder-artifact sirdavidludwig/dnabert-pretrain/dnabert-pretrain-64dim:latest --wandb-project "StrXL_Induced" --save_to "StrXL_Induced_20_Samples_Run1" --gpus 0
+python3 StrXL_Induced_Training.py --dataset-artifact sirdavidludwig/nachusa-dna/dnasamples-complete:latest --encoder-artifact sirdavidludwig/dnabert-pretrain/dnabert-pretrain-64dim:latest --wandb-project "StrXL_Induced" --save_to "StrXL_Induced_20_0_Run1" --gpus 0 --seed 0
